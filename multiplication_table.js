8fc1b07e28fc7c95b0e3c49167f526ac102f6f1c
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-09-19 13:08:31 +0530

    Create  multiplication_table.js

---
+++ multiplication_table.js
@@ -0,0 +1,5 @@
+const multiplicand = 2;
+for(let multiplier = 1;multiplier <= 10;multiplier ++){
+  let product = multiplicand * multiplier;
+  console.log(multiplicand," * ",multiplier ,"=" ,  product);
+}
