884d3e294a7142d26015f9c566e649554d50271e
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-09-21 17:38:16 +0530

    Create reverse_of_a_number.js
    display the reverse of the given number

---
+++ reverse_of_a_number.js
@@ -0,0 +1,10 @@
+const number = 30
+let reverseNumber = 0;
+let copyNumber = number;
+let lastdigit =0;
+while(copyNumber > 0) {
+  lastdigit = copyNumber % 10;
+  copyNumber = (copyNumber-lastdigit)/10;
+  reverseNumber =  reverseNumber * 10 + lastdigit;
+}
+console.log(number," reverse =", reverseNumber);
