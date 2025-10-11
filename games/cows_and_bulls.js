3304f17a8cff28c2467fa9c39c296e4ad16d07cd
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-10-11 18:10:39 +0530

    Update cows_and_bulls.js

--- games/cows_and_bulls.js
+++ games/cows_and_bulls.js
@@ -15,22 +15,45 @@ function generateSecreateNumber() {
 function getGuess() {
   return prompt("enter your guess");
 }
-function composeMessage() {
+
+function composeWinMessage() {
   console.log("-".repeat(21))
   console.log('\n CONGRATULATIONS! 🎉 \n You Guessed The Number 🥳\n');
   console.log("-".repeat(22));
 }
+
 function checkGuess(guess, secreateNumber) {
-  if (secreateNumber === guess) {
-    composeMessage();
+  let cowsAndBulls = [0, 0];
+  for (let index = 0; index < guess.length; index++) {
+    if (guess[index] === secreateNumber[index]) {
+      cowsAndBulls[1]++;
+    } else if (secreateNumber.includes(guess[index])) {
+      cowsAndBulls[0]++;
+    }
   }
+  return cowsAndBulls;
+}
+function NoOfAttempts() {
+  let message = "set Your Challenge!";
+  message += "How Many Attempts Will you take";
+  message += " to find the secrete Number";
+  return message;
 }
 
 function playCowsAndBulls() {
   const secreateNumber = generateSecreateNumber();
+  let attempts = parseInt(prompt(NoOfAttempts()));
   console.log(secreateNumber);
-  const guess = getGuess();
-  checkGuess(guess, secreateNumber);
+  while (attempts > 0) {
+    const guess = getGuess();
+    const result = checkGuess(guess, secreateNumber);
+    if (result[1] === 4) {
+      composeWinMessage();
+      break;
+    }
+    console.log(`${result[0]} cows | ${result[1]} bulls`);
+    attempts--;
+  }
 }
 
 playCowsAndBulls();
