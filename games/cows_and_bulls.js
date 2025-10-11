3a6b37bc3c49a2414a738396b6c2ddd0bfe721c4
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-10-11 17:15:33 +0530

    Update cows_and_bulls.js

--- games/cows_and_bulls.js
+++ games/cows_and_bulls.js
@@ -1,1 +1,36 @@
+function getRandomDigit() {
+  return Math.floor(Math.random() * 10);
+}
+function generateSecreateNumber() {
+  let digits = [];
+  while (digits.length < 4) {
+    const randomDigit = getRandomDigit();
+    if (!digits.includes(randomDigit)) {
+      digits.push(randomDigit);
+    }
+  }
+  return digits.join("");
+}
 
+function getGuess() {
+  return prompt("enter your guess");
+}
+function composeMessage() {
+  console.log("-".repeat(21))
+  console.log('\n CONGRATULATIONS! 🎉 \n You Guessed The Number 🥳\n');
+  console.log("-".repeat(22));
+}
+function checkGuess(guess, secreateNumber) {
+  if (secreateNumber === guess) {
+    composeMessage();
+  }
+}
+
+function playCowsAndBulls() {
+  const secreateNumber = generateSecreateNumber();
+  console.log(secreateNumber);
+  const guess = getGuess();
+  checkGuess(guess, secreateNumber);
+}
+
+playCowsAndBulls();
