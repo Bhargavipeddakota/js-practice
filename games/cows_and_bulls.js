e589aabf37324e3dd4ac41212dedf33d2222fa8e
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-10-11 19:00:20 +0530

    Update cows_and_bulls.js

--- games/cows_and_bulls.js
+++ games/cows_and_bulls.js
@@ -13,12 +13,19 @@ function generateSecreateNumber() {
 }
 
 function getGuess() {
-  return prompt("enter your guess");
+  return prompt(" enter your guess\n");
 }
 
 function composeWinMessage() {
   console.log("-".repeat(21))
   console.log('\n CONGRATULATIONS! 🎉 \n You Guessed The Number 🥳\n');
+  console.log("-".repeat(22));
+}
+
+function composeLoosMessage(attempts, secreateNumber) {
+  console.log("-".repeat(21))
+  console.log(`\n Game Over \n You used All ${attempts} attempts`);
+  console.log(` The SecretNumber was ${secreateNumber}\n`);
   console.log("-".repeat(22));
 }
 
@@ -33,26 +40,31 @@ function checkGuess(guess, secreateNumber) {
   }
   return cowsAndBulls;
 }
+
 function NoOfAttempts() {
-  let message = "set Your Challenge!";
-  message += "How Many Attempts Will you take";
-  message += " to find the secrete Number";
+  let message = "\n 🎯 Set Your Challenge ! \n";
+  message += " How Many Attempts Will you take";
+  message += " to find the secrete Number : ";
   return message;
 }
 
 function playCowsAndBulls() {
   const secreateNumber = generateSecreateNumber();
-  let attempts = parseInt(prompt(NoOfAttempts()));
-  console.log(secreateNumber);
-  while (attempts > 0) {
+  let attempts = 0;
+  const maxAttempts = parseInt(prompt(NoOfAttempts()));
+  console.log(`\n A Secret Number has been chosen \n you have ${maxAttempts} attempts\n`);
+  while (attempts < maxAttempts) {
     const guess = getGuess();
     const result = checkGuess(guess, secreateNumber);
     if (result[1] === 4) {
       composeWinMessage();
       break;
     }
-    console.log(`${result[0]} cows | ${result[1]} bulls`);
-    attempts--;
+    console.log(`\n ${result[0]} cows | ${result[1]} bulls\n`);
+    attempts++;
+  }
+  if (attempts === maxAttempts) {
+    composeLoosMessage(maxAttempts, secreateNumber);
   }
 }
 
