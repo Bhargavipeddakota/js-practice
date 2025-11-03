afc09126c611e7c0746cce97a241538d68998ce0
Bhargavipeddakota <bhargavicme22040@gmail.com>
2025-11-03 12:34:29 +0530

    Update bencode.js

--- bencode.js
+++ bencode.js
@@ -1,17 +1,22 @@
-function isNumber(data) {
-  return typeof data === "number";
-}
-
-function isString(data) {
-  return typeof data === "string";
+function encodeArray(data) {
+  let result = "l";
+  for (let index = 0; index < data.length; index++) {
+    result += encode(data[index]);
+  }
+  return result + "e";
 }
 
 function encode(data) {
-  if (isNumber(data)) {
-    return `i${data}e`;
-  }
-  if (isString(data)) {
-    return `${data.length}:${data}`;
+  switch (typeof data) {
+    case "number":
+      return `i${data}e`;
+    case "string":
+      return `${data.length}:${data}`;
+    case "object":
+      const result = encodeArray(data);
+      return result;
+    default:
+      break;
   }
 }
 
@@ -28,12 +33,15 @@ function testEncode(data, expected, description) {
   console.log(message);
 }
 
-function testAllTestCases() {
+function testAllEncode() {
   testEncode(0, "i0e", "zero");
   testEncode(1, "i1e", "Positive Integer");
   testEncode(-1, "i-1e", "Negative Integer");
   testEncode("a", "1:a", "simple string");
   testEncode("", "0:", "empty string");
+  testEncode(["a"], "l1:ae", "single element array");
+  testEncode(["a", 1], "l1:ai1ee", "single element array");
+  testEncode(["a", [1]], "l1:ali1eee", "2 dimensional array");
 }
 
-testAllTestCases();
+testAllEncode();
