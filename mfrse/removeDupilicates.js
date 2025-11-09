14fa18814115d86c71365a4f53b9db8e38a84d30
Peddakota Bhargavi <peddakotabhargavi@Peddakotas-MacBook-Pro.local>
2025-11-09 17:40:13 +0530

    update remove Dupicate function

--- mfrse/removeDupilicates.js
+++ mfrse/removeDupilicates.js
@@ -1,12 +1,16 @@
-const isIncludes = (x,index,array) => array.indexOf(x) === index;
-
-const removeDupilicates = array => array.flat().filter(isIncludes);
-
+const reducer = function(array,currentElement){
+  if(!array.includes(currentElement)){
+    array.push(currentElement);
+  }
+  return array;
+}
 const array = [["Orion", "Leo"],["Taurus"],["Orion", "Gemini","Orion"],["Gemini"] ];
-console.log(removeDupilicates(array));
+const removeDuplicates = array => array.flat().reduce(reducer,[]);
+
+console.log(removeDuplicates(array));
 
 const birds = ["sparrow", "eagle","crow", "sparrow", "eagle", "crow"];
-console.log(removeDupilicates(birds));
+console.log(removeDuplicates(birds));
 
 const studentNames = [["Asha", "Ravi", "Neel"],["Ravi"],["Asha", "Meera"]];
-console.log(removeDupilicates(studentNames));
+console.log(removeDuplicates(studentNames));
