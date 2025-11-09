2a48b176364c2145a96f50a68c715a18f7982016
Peddakota Bhargavi <peddakotabhargavi@Peddakotas-MacBook-Pro.local>
2025-11-09 12:37:31 +0530

    detete file

--- mfrse/reduce.js
+++ mfrse/reduce.js
@@ -1,6 +1,4 @@
 const ribbonColors = ["red", "blue", "red", "green", "red", "blue"];
-const numberOfBlue = function (count,color) {
-  return color === "blue" ? count + 1 : count;
-}
+const numberOfBlue = (count,color) => color === "blue" ? count + 1 : count;
 const colorCount = ribbonColors.reduce(numberOfBlue,0);
 console.log(colorCount);
